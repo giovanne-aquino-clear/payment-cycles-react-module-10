8032709513c7c05024326e0b8678deebded00427
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-27 10:37:43 -0300

    adds error handling

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -1,15 +1,17 @@
 const BillingCycle = require('./billingCycle')
+const errorHandler = require('../common/errorHandler')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({new:true, runValidators: true})
+BillingCycle.updateOptions({ new: true, runValidators: true })
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
 
 
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
-        if(error) {
-            res.status(500).json({errors: [error]})
+        if (error) {
+            res.status(500).json({ errors: [error] })
         } else {
-            res.json({value})
+            res.json({ value })
         }
     })
 })
@@ -19,13 +21,13 @@ BillingCycle.route('get', (req, res, next) => {
 
     BillingCycle.find({}, (err, docs) => {
 
-        if(!err) {
+        if (!err) {
 
             res.json(docs)
 
         } else {
 
-            res.status(500).json({errors: [err]})
+            res.status(500).json({ errors: [err] })
 
         }
 
@@ -34,17 +36,17 @@ BillingCycle.route('get', (req, res, next) => {
 })
 
 BillingCycle.route('summary', (req, res, next) => {
-    BillingCycle.aggregate([{ 
-        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}} 
-    }, { 
-        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
-    }, { 
-        $project: {_id: 0, credit: 1, debt: 1}
+    BillingCycle.aggregate([{
+        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
+    }, {
+        $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+    }, {
+        $project: { _id: 0, credit: 1, debt: 1 }
     }], (error, result) => {
-        if(error) {
-            res.status(500).json({errors: [error]})
+        if (error) {
+            res.status(500).json({ errors: [error] })
         } else {
-            res.json(result[0] || {credit: 0, debt: 0})
+            res.json(result[0] || { credit: 0, debt: 0 })
         }
     })
 })
